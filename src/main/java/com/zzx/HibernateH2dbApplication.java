bf9e79a77f1b09446d57a6a8662504c186aa1f3d
zzx <piofcircle@gmail.com>
2017-06-06 22:45:52 +0800

    init

---
+++ src/main/java/com/zzx/HibernateH2dbApplication.java
@@ -0,0 +1,12 @@
+package com.zzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateH2dbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateH2dbApplication.class, args);
+	}
+}
